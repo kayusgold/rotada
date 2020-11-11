50dc86eebfb38a0dcd5d48ac05dd3c71ec6c2660
kayusgold <kayodeomolaoye@gmail.com>
2020-11-11 10:33:44 +0100

    Adds Deployer

---
+++ deploy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'recipe/rsync.php';
+
+set('application', 'My App');
+set('ssh_multiplexing', true);
+
+set('rsync_src', function () {
+    return __DIR__;
+});
+
+
+add('rsync', [
+    'exclude' => [
+        '.git',
+        '/.env',
+        '/storage/',
+        '/vendor/',
+        '/node_modules/',
+        '.github',
+        'deploy.php',
+    ],
+]);
+
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+host('eazibank.ml')
+    ->hostname('162.0.233.202')
+    ->stage('production')
+    ->user('eazibank')
+    ->set('deploy_path', '/public_html');
+
+// host('staging.myapp.io')
+//     ->hostname('104.248.172.220')
+//     ->stage('staging')
+//     ->user('root')
+//     ->set('deploy_path', '/var/www/my-app-staging');
+
+after('deploy:failed', 'deploy:unlock');
+
+desc('Deploy the application');
+
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    'deploy:secrets',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'artisan:queue:restart',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
