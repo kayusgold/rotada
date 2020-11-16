48d05ed006a239bfed8ba9027d9f3e4e9a9d7834
kayusgold <kayodeomolaoye@gmail.com>
2020-11-16 12:13:44 +0100

    Adjusts Login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -50,6 +50,7 @@ class AuthController extends BaseController
      */
     public function login(Request $request)
     {
+        Log::info("Login: ".json_encode($request->input()));
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = Auth::user();
             $token = $user->createToken('MyApp')->accessToken;
